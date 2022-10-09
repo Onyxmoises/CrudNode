201df711324f0bb679971b72c41b50af846a5262
Onyxmoises <mmorenor2003@alumno.ipn.mx>
2022-10-09 17:23:33 -0500

    correccion port

--- database/db.js
+++ database/db.js
@@ -3,12 +3,14 @@ const BD_HOST = process.env.BD_HOST || 'localhost';
 const BD_USER = process.env.BD_USER || 'root';
 const BD_PASSWORD = process.env.BD_PASSWORD || 'n0m3l0';
 const BD_NAME = process.env.BD_NAME || 'bd';
+const BD_PORT = process.env.BD_PORT || 'bd';
 
 let con = mysql.createConnection({
     host: BD_HOST,
     user: BD_USER,
     password: BD_PASSWORD,
-    database: BD_NAME
+    database: BD_NAME,
+    port: BD_PORT
 });
 
 con.connect((error)=>{
